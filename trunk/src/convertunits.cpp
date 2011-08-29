852919ed5a33ae4c581cc210730bff5f96e7f73f
daniel.genrich@gmail.com <daniel.genrich@gmail.com@95fb0d15-58e6-cb90-d63b-2718a2855067>
2011-08-29 12:43:38 +0000

    Fix crash while using route, fix crash in gpsd debug. TODO: Route doesn't go until end

--- trunk/src/convertunits.cpp
+++ trunk/src/convertunits.cpp
@@ -93,6 +93,10 @@ double pixel_to_latitude(double zoom, int pixel_y)
 double fast_distance_rad(double lat1, double lon1, double lat2, double lon2)
 {
 	double dist = acos(sin(lat1)*sin(lat2)+cos(lat1)*cos(lat2)*cos(lon2-lon1))*6371.0;
+
+	if(isnan(dist))
+		dist = 0;
+
     return dist;
 }
 
